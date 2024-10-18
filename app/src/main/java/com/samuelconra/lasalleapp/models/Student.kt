139e313a63ca5b8bf00950af97a9daf8e5ad8bd5
Samuel Conde <samuelconra@192.168.0.245>
2024-10-18 12:14:59 -0600

    Student & Subject Created

--- app/src/main/java/com/samuelconra/lasalleapp/models/Student.kt
+++ app/src/main/java/com/samuelconra/lasalleapp/models/Student.kt
@@ -1,3 +1,18 @@
 package com.samuelconra.lasalleapp.models
 
-data class Student()
+data class Student(
+    val name: String,
+    val firstSurname: String,
+    val secondSurname: String,
+    val email: String,
+    val career: String,
+    val enrollment: Int,
+    val subjects: List<Subject>
+) {
+    val average: Double
+        get() = if (subjects.isNotEmpty()) {
+            subjects.sumOf { it.average } / subjects.size
+        } else {
+            0.0
+        }
+}
